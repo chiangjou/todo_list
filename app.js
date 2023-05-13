cd25d448d84ee1e6cc4b82ca3242b7a3d52679ce
chiangjou <tiffanyj.chiang@gmail.com>
2023-05-13 18:43:43 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-// 載入 express 並建構應用程式伺服器
 const express = require('express')
-const mongoose = require('mongoose')  // 載入 mongoose
+const mongoose = require('mongoose')
+const exphbs = require('express-handlebars')
 
 // 僅在非正式環境時, 使用 dotenv
 if (process.env.NODE_ENV !== 'production') {
@@ -23,11 +23,13 @@ db.once('open', () => {
 
 // 設定首頁路由
 app.get('/', (req, res) => {
-  res.send('hello!')
+  res.render('index')
 })
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
 
 // 設定 port 3000
 app.listen(3000, () => {
   console.log('App is running on http://localhost:3000')
 })
-
