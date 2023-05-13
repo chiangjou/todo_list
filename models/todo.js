e89c96151572545e2b82a08923de507e9161aef7
chiangjou <tiffanyj.chiang@gmail.com>
2023-05-13 18:27:29 +0800

    feat: add todo seeder

--- models/todo.js
+++ models/todo.js
@@ -3,10 +3,10 @@ const Schema = mongoose.Schema
 const todoSchema = new Schema({
   name: {
     type: String, // 資料型別是字串
-    required: true // 這個是必填欄位
+    required: true // 這是個必填欄位
   },
   done: {
     type: Boolean
   }
 })
-module.exports = mongoose.model('Todo, todoSchema')
+module.exports = mongoose.model('Todo', todoSchema)
